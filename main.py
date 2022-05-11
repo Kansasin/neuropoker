9fe9a95f4eb3bb33d645e88fab5262664d1813b2
konsantin.zinovev <konsantin.zinovev@ananas-web.ru>
2022-05-11 19:49:53 +0500

    moved terminal color setting to main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from poker import *
 from interface import *
 
 if __name__ == '__main__':
+    os.system('color 0A')  # 0A - черный фон, зеленый текст, F0 - белый фон, черный текст
+    os.system('title НейроПокер')
+    # os.system("mode con cols=75 lines=25")
     start()
