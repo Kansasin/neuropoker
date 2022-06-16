352cf7ff15aa4d0ed0563df9825938e4b64d3c4f
konsantin.zinovev <konsantin.zinovev@ananas-web.ru>
2022-06-16 05:06:53 +0500

    added brains saving/loading methods

--- file.py
+++ file.py
@@ -17,3 +17,13 @@ class File:
             with open('./train_settings.json', 'r') as f:
                 for key, value in json.loads(f.read()).items():
                     train_settings.SAVABLE[key] = value
+
+    @staticmethod
+    def save_brains(train_settings):
+        with open(train_settings.SAVABLE['IMPORT_PATH'], 'w+') as f: f.write(json.dumps(train_settings.SAVABLE['BEST_BRAINS']))
+
+    @staticmethod
+    def load_brains(train_settings):
+        if os.path.exists(train_settings.SAVABLE['EXPORT_PATH']):
+            with open(train_settings.SAVABLE['EXPORT_PATH'], 'r') as f:
+                train_settings.SAVABLE['BEST_BRAINS'] = json.loads(f.read())
